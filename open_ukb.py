f64fc54c3d4445765dfcf35e90583863d665352b
Idriss-Design <idrissdjialeu5@gmail.com>
2026-04-01 22:57:03 +0100

    Modification du fichier open_ukb.py

--- open_ukb.py
+++ open_ukb.py
@@ -1,14 +1,9 @@
 import pickle
 
-# On "décongèle" les données
-print("Chargement du fichier binaire...")
-with open('ukb_data.pkl', 'rb') as f:
-    kb_charge = pickle.load(f)
+# Ouvrir le fichier
+with open("ukb_structured.pkl", "rb") as f:
+    data = pickle.load(f)
 
-# On vérifie que tout est là
-nb_sujets = len(kb_charge)
-print(f"Le fichier contient {nb_sujets} sujets.")
-
-# On affiche un exemple au hasard
-un_sujet = list(kb_charge.keys())[0]
-print(f"Données pour {un_sujet} : {kb_charge[un_sujet]}")
+# Vérifier ce que contient
+print(type(data))  # pour voir le type de l'objet
+print(data)        # afficher (attention si c'est très gros)
